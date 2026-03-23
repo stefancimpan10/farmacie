2c8f36bfd649bc67414eb3eda04825dbc24121dc
stefancimpan10 <iqzohjmk@gmail.com>
2026-03-23 11:36:32 +0200

    Reordonare

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FarmacieApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager farmacie = new Manager();
+            bool ruleaza = true;
+
+            while (ruleaza)
+            {
+                Console.WriteLine("\n=== PROGRAM GESTIUNE FARMACIE ===");
+                Console.WriteLine("1. Vizualizare Catalog");
+                Console.WriteLine("2. Adaugare Produs Nou");
+                Console.WriteLine("3. Actualizare Stoc (Intrari/Iesiri)");
+                Console.WriteLine("4. Verificare Produse Expirate");
+                Console.WriteLine("5. Creare Comanda Noua (Vanzare)");
+                Console.WriteLine("6. Cautare Rapida (Nume)");
+                Console.WriteLine("7. Vizualizare Istoric Vanzari");
+                Console.WriteLine("8. Stergere Produs");
+                Console.WriteLine("9. Modificare Pret");
+                Console.WriteLine("X. Iesire");
+                Console.Write("\nAlegeti o optiune: ");
+
+                string optiune = Console.ReadLine().ToUpper();
+
+                switch (optiune)
+                {
+                    case "1": 
+                        farmacie.ObtineMeniu(); 
+                        break;
+                    case "2": 
+                        farmacie.AdaugaProdus(); 
+                        break;
+                    case "3":
+                        Console.Write("ID Produs: "); int idS = int.Parse(Console.ReadLine());
+                        Console.Write("Cantitate de adaugat/scas (+/-): "); int cantS = int.Parse(Console.ReadLine());
+                        farmacie.ActualizeazaStoc(idS, cantS);
+                        break;
+                    case "4": 
+                        farmacie.VerificaExpirate(); 
+                        break;
+                    case "5": 
+                        farmacie.AdaugaComanda(); 
+                        break;
+                    case "6":
+                        Console.Write("Introduceti textul pentru cautare: ");
+                        string cauta = Console.ReadLine();
+                        farmacie.CautaRapid(cauta);
+                        break;
+                    case "7": 
+                        farmacie.ObtineComenzi(); 
+                        break;
+                    case "8":
+                        Console.Write("ID Produs de sters: ");
+                        int idDel = int.Parse(Console.ReadLine());
+                        farmacie.StergeProdus(idDel);
+                        break;
+                    case "9":
+                        Console.Write("ID Produs: "); int idP = int.Parse(Console.ReadLine());
+                        Console.Write("Pret nou: "); decimal prN = decimal.Parse(Console.ReadLine());
+                        farmacie.ModificaPret(idP, prN);
+                        break;
+                    case "X": 
+                        ruleaza = false; 
+                        break;
+                    default: 
+                        Console.WriteLine("Optiune invalida! Incearca din nou."); 
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,59 @@
+Operatii care vor fi in aplicatie : 
+- [1] Vizualizare Catalog
+- [2] Adăugare Produs Nou
+- [3] Actualizare Stoc:
+- [4] Verificare Produse Expirate
+- [5] Creare Comandă Nouă
+- [6] Căutare Rapidă # Cautare dupa nume sau substanta
+- [7] Vizualizare Istoric Vânzări
+- [8] Stergere/Dezactivare produs
+- [9] Modificare pret
+- [x] Iesire
+
+Se vor stoca informatiile pentru fiecare medicament : 
+- ID Produs
+- Denumire comercială
+- Substanță activă
+- Formă farmaceutică
+- Concentrație
+- Preț unitar
+- Stoc disponibil
+- Necesită Rețetă
+- Data Expirării
+
+Se vor stoca informatiile pentru fiecare comanda : 
+- ID Produs
+- Data si ora
+- Lista articole :
+        - ID Produs
+        - Cantitatea cumparata 
+        - Pret la momentul vanzarii
+- Date client :
+        - Nume si prenume
+        - Numar de telefon
+- Daca necesita reteta :
+        - Cod parafa medic
+        - Serie/Numar reteta
+- Total plata
+- Metoda de Plata
+- Status plata
+
+
+Clasele necesare implementării aplicației:
+    -  Clasa Produs: Reprezintă medicamentul. Conține toate detaliile tehnice, prețul și informațiile de siguranță (rețetă, expirare).
+
+    - Clasa ArticolComanda: Este elementul de legătură. Conține un obiect de tip Produs și cantitatea dorită, calculând automat prețul pentru acea linie din comandă.
+
+    - Clasa Comanda: Reprezintă tranzacția finală. Stochează datele clientului, lista de articole, totalul de plată și detaliile rețetei dacă produsul a fost marcat ca fiind restrictiv.
+
+    - Clasa Manager: Este centrul de comandă al aplicației. Gestionează listele globale (stocul și istoricul) și execută toate operațiile de adăugare, ștergere sau căutare.
+
+    Ar arata ceva :
+
+Clasa Produs: Constructor, GetID, SetID, GetDenumire, SetDenumire, GetSubstanta, SetSubstanta, GetForma, SetForma, GetConcentratie, SetConcentratie, GetPret, SetPret, GetStoc, SetStoc, GetReteta, SetReteta, GetDataExpirarii, SetDataExpirarii.
+
+Clasa ArticolComanda: Constructor, GetProdus, SetProdus, GetCantitate, SetCantitate, GetPretTotalArticol.
+
+Clasa Comanda: Constructor, GetID, SetID, GetNumeClient, SetNumeClient, GetProduse, SetProduse, GetPretTotal, GetDataOra, SetDataOra, GetStatusPlata, SetStatusPlata, GetCodParafa, SetCodParafa, AdaugaArticol.
+
+Clasa Manager: AdaugaProdus, StergeProdus, ModificaPret, ActualizeazaStoc, ObtineMeniu, CautaProdus, CautaRapid, VerificaExpirate, AdaugaComanda, ObtineComenzi, CautaComanda, ModificaComanda, StergeComanda.
